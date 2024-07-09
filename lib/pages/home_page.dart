dba4347ec16564e04367464a5365e255d9ceb318
Saniya951 <dangatsaniya050@gmail.com>
2024-07-10 02:28:03 +0530

    UI final updated of home page seperately

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,10 @@
-import 'package:eco_resolve/constants/btn.dart';
 import 'package:eco_resolve/constants/colors.dart';
 import 'package:eco_resolve/constants/size.dart';
 import 'package:eco_resolve/widgets/drawer_mob.dart';
 import 'package:eco_resolve/widgets/header.dart';
 import 'package:eco_resolve/widgets/header_mob.dart';
+import 'package:eco_resolve/widgets/home_desk.dart';
+import 'package:eco_resolve/widgets/home_mob.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -17,31 +18,6 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     final scaffoldKey = GlobalKey<ScaffoldState>();
-
-    // Define text styles
-    const TextStyle desktopTitleStyle = TextStyle(
-      fontWeight: FontWeight.bold,
-      fontSize: 20,
-      color: CustomColor.greenSecondary,
-    );
-
-    const  TextStyle mobileTitleStyle = TextStyle(
-      fontWeight: FontWeight.bold,
-      fontSize: 14,
-      color: CustomColor.greenSecondary,
-    );
-
-    const TextStyle desktopBodyStyle = TextStyle(
-      fontWeight: FontWeight.w400,
-      fontSize: 15,
-      color: CustomColor.lightGrey,
-    );
-
-    const TextStyle mobileBodyStyle = TextStyle(
-      fontWeight: FontWeight.w400,
-      fontSize: 8,
-      color: CustomColor.lightGrey,
-    );
 
     // LayoutBuilder for adding responsive page constraints
     return LayoutBuilder(
@@ -59,7 +35,7 @@ class _HomePageState extends State<HomePage> {
           body: ListView(
             scrollDirection: Axis.vertical,
             children: [
-              // MAIN HOME PAGE
+              // MAIN HEADER PAGE
               if (isDesktop)
                 const Header()
               else
@@ -71,118 +47,14 @@ class _HomePageState extends State<HomePage> {
                   },
                 ),
 
-              Container(
-                margin:const EdgeInsets.symmetric(horizontal: 20.0),
-                constraints:BoxConstraints(maxHeight:screenSize.height*0.45),
-                child:Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Expanded(
-                      child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Welcome to our Community Service and Social Responsibility Platform",
-                              style: isDesktop ? desktopTitleStyle : mobileTitleStyle,
-                            ),
-                            const SizedBox(height: 8.0),
-                            SizedBox(
-                              width: isDesktop? 600.0 : null,
-                              child: Text(
-                                "We are dedicated to promoting social responsibility and community engagement. Our platform connects volunteers, organizations, and community members who share a passion for making a positive impact. Together, we can create meaningful change and build a stronger, more compassionate community.",
-                                style: isDesktop ? desktopBodyStyle : mobileBodyStyle,
-                                maxLines: 8,  // Conditionally set maxLines
-                                // overflow: TextOverflow.ellipsis,
-                              ),
-                            ),
-                      
-                            const SizedBox(height: 8.0), //for space
-                      
-                            // import custom btn form constants
-                            CustomButton(
-                              text: "Community Service",
-                              onPressed: () {},
-                              constraints: constraints,
-                            ),
-                             
-                          ],
-                        ),
-                    ),
-                    
-                    const SizedBox(height: 8.0), 
-                    
-                    Padding(
-                      padding: isDesktop? const EdgeInsets.only(right:80.0):const EdgeInsets.only(right:5.0),
-                      child: Image.asset(
-                          "assets/images/EcoResolve-logo.png",
-                          width: isDesktop ? 200.0 : 100.0, // Adjust the width
-                          height: isDesktop ? 200.0 : 100.0, ),
-                    ),
-                  
-                  ],
-                ),
-              ),
+
+              //MAIN HOME PAGE
+              if (isDesktop)
+                const HomeDesk()
+              else
+                const HomeMob(),
 
 
-              Container(
-                margin:const EdgeInsets.symmetric(horizontal: 20.0),
-                constraints:BoxConstraints(maxHeight:screenSize.height*0.45),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    // SizedBox(height: isDesktop ? 100.0 : 16.0),
-                    
-                    Padding(
-                      padding: isDesktop? const EdgeInsets.only(left:80.0):const EdgeInsets.only(left:5.0),
-                      child: Image.asset(
-                        "assets/images/EcoResolve-logo.png",
-                        width: isDesktop ? 200.0 : 100.0, // Adjust the width
-                        height: isDesktop ? 200.0 : 100.0, ),
-                    ),
-
-                    const SizedBox(height: 16.0), 
-                    
-
-
-                    Expanded(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          Text(
-                            "Welcome to our Conflict Resolution Service Platform",
-                            style: isDesktop ? desktopTitleStyle : mobileTitleStyle,
-                            textAlign: TextAlign.right,
-                          ),
-                          const SizedBox(height: 8.0),
-                          SizedBox(
-                            width: isDesktop? 800.0 : null,
-                            child: Text(
-                              "We are dedicated to promoting social responsibility and community engagement. Our platform connects volunteers, organizations, and community members who share a passion for making a positive impact. Together, we can create meaningful change and build a stronger, more compassionate community.",
-                              style: isDesktop ? desktopBodyStyle : mobileBodyStyle,
-                              textAlign: TextAlign.right,
-                              maxLines: 8,  // Conditionally set maxLines
-                              // overflow: TextOverflow.ellipsis,
-                            ),
-                          ),
-
-                          const SizedBox(height: 8.0), //for space
-
-                          // import custom btn form constants
-                          CustomButton(
-                            text: "Conflict Resolution",
-                            onPressed: () {},
-                            constraints: constraints,
-                          ),
-                           
-                        ],
-                      ),
-                    ),
-                  
-                  ],
-                ),
-              ),
 
               // Conflict resolution Page
               Container(
@@ -211,3 +83,6 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
+        
+        
+    
---
+++ lib/widgets/home_desk.dart
@@ -0,0 +1,173 @@
+import 'package:eco_resolve/constants/colors.dart';
+import 'package:flutter/material.dart';
+
+class HomeDesk extends StatelessWidget {
+  const HomeDesk({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+    return Container(
+      child: Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 20.0),
+            constraints: BoxConstraints(maxHeight: screenSize.height * 0.45),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        "Welcome to our Community Service and Social Responsibility Platform",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: CustomColor.greenSecondary,
+                        ),
+                      ),
+                      const SizedBox(height: 8.0),
+                      const SizedBox(
+                        width: 600.0,
+                        child: Text(
+                          "We are dedicated to promoting social responsibility and community engagement. Our platform connects volunteers, organizations, and community members who share a passion for making a positive impact. Together, we can create meaningful change and build a stronger, more compassionate community.",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                            fontSize: 15,
+                            color: CustomColor.lightGrey,
+                          ),
+                          maxLines: 8, // Conditionally set maxLines
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                      const SizedBox(height: 16.0), // for space
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 25.0,
+                            vertical: 20.0,
+                          ),
+                          textStyle: const TextStyle(fontSize: 15),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12.0), // Border radius for rectangular curve
+                            side: const BorderSide(
+                              color: CustomColor.greenSecondary, // Border color
+                              width: 1.0, // Border width
+                            ),
+                          ),
+                        ),
+                        child: const Text(
+                          "Conflict Resolution",
+                          style: TextStyle(color: CustomColor.greenSecondary),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(width: 16.0), // space between text and image
+                Padding(
+                  padding: const EdgeInsets.only(right: 80.0),
+                  child: Image.asset(
+                    "assets/images/EcoResolve-logo.png",
+                    width: 200.0, // Adjust the width
+                    height: 200.0,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          
+
+
+          Container(
+                margin:const EdgeInsets.symmetric(horizontal: 20.0),
+                constraints:BoxConstraints(maxHeight:screenSize.height*0.45),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    // SizedBox(height: isDesktop ? 100.0 : 16.0),
+                    
+                    Padding(
+                      padding: const EdgeInsets.only(left:80.0),
+                      child: Image.asset(
+                        "assets/images/EcoResolve-logo.png",
+                        width: 200.0 , // Adjust the width
+                        height:200.0, ),
+                    ),
+
+                    const SizedBox(height: 16.0), 
+                    
+
+
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          const Text(
+                            "Welcome to our Conflict Resolution Service Platform",
+                            style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: CustomColor.greenSecondary,
+                        ),
+                            textAlign: TextAlign.right,
+                          ),
+                          const SizedBox(height: 8.0),
+                          const SizedBox(
+                            width: 800.0,
+                            child: Text(
+                              "We are dedicated to promoting social responsibility and community engagement. Our platform connects volunteers, organizations, and community members who share a passion for making a positive impact. Together, we can create meaningful change and build a stronger, more compassionate community.",
+                              style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                            fontSize: 15,
+                            color: CustomColor.lightGrey,
+                          ),
+                              textAlign: TextAlign.right,
+                              maxLines: 8,  // Conditionally set maxLines
+                              // overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+
+                          const SizedBox(height: 8.0), //for space
+
+                          // import custom btn form constants
+                          ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 25.0,
+                            vertical: 20.0,
+                          ),
+                          textStyle: const TextStyle(fontSize: 15),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12.0), // Border radius for rectangular curve
+                            side: const BorderSide(
+                              color: CustomColor.greenSecondary, // Border color
+                              width: 1.0, // Border width
+                            ),
+                          ),
+                        ),
+                        child: const Text(
+                          "Community Service",
+                          style: TextStyle(color: CustomColor.greenSecondary),
+                        ),
+                      ),
+                           
+                        ],
+                      ),
+                    ),
+                  
+                  ],
+                ),
+              ),
+
+        ],
+      ),
+    );
+  }
+}
